28204370c6f59efd411d7f451385107461a2e924
raamkumar124 <53335308+raamkumar124@users.noreply.github.com>
2019-08-10 12:04:53 +0530

    Add files via upload

---
+++ divide by 2 array.py
@@ -0,0 +1,12 @@
+x=int(input())
+li=list(map(int,input().split()))
+c=0
+for i in range(1,len(li)):
+    if (sum(li[:i]))//len(li[:i])==(sum(li[i:]))//len(li[i:]):
+        c+=1
+    else:
+        c=0
+if c>=1:
+    print("yes")
+else:
+    print("no")
